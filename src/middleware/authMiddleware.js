b0a640bf474b7ad0ddd2af5f8972d6cb37c35cab
HoangPhuc2707 <tranp8639@gmail.com>
2024-08-26 23:09:04 +0700

    handle-token-expired

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -26,8 +26,9 @@ const authUserMiddleWare = (req, res, next) => {
     const token = req.headers.token.split(' ')[1]
     const userId = req.params.id
     jwt.verify(token, process.env.ACCESS_TOKEN, function (err, user) {
+        
         if (err) {
-            return res.status(404).json({
+            return res.status(401).json({
                 message: 'The authemtication',
                 status: 'ERROR'
             })
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -66,7 +66,6 @@ const loginUser = (userLogin) => {
                 id: checkUser.id,
                 isAdmin: checkUser.isAdmin
             })
-
             resolve({
                 status: 'OK',
                 message: 'SUCCESS',
